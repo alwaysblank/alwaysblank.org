90294b3bb2cae3efa2f32a1da82ff6042c6e232f
Ben Martinez-Bateman <ben@alwaysblank.org>
2020-12-06 12:57:16 -0800

    Improvments to content blocks

--- 11ty/filters/markdown.js
+++ 11ty/filters/markdown.js
@@ -1,7 +1,22 @@
 const md = require('markdown-it');
 
 module.exports = conf => {
-    conf.addFilter('md', (string) => {
-        return (new md).render(string);
+    conf.addFilter('md', (string, opts) => {
+        const {
+            p
+        } = Object.assign({
+            p: true
+        }, opts)
+        const engine = new md();
+        if (false === p) {
+            /**
+             * The primary purpose here is to disable <p> tags, i.e.
+             * text inside of other tags. You should only use this option
+             * if you're relatively certain that the text will never
+             * actually *need* paragraphs.
+             */
+            return engine.renderInline(string)
+        }
+        return engine.render(string);
     })
 }
